30fb2ecfc6e0db6cf8b1bac8b0f47006d9a333d4
Srinivasa Chari <srinivaschari6268@gmail.com>
2024-08-21 17:42:32 +0530

    adding colours

--- app/src/main/java/com/chari6268/jetpackapp/ui/theme/Color.kt
+++ app/src/main/java/com/chari6268/jetpackapp/ui/theme/Color.kt
@@ -10,3 +10,18 @@ val Teal200 = Color(0xFF03DAC5)
 val MediumGray = Color(0xFF2E2E2E)
 val LightGray = Color(0xFF818181)
 val Orange = Color(0xFFFF9800)
+
+val PurpleLite = Color(0xFFDACBFA)  // #DACBFA
+val Purple = Color(0xFFBA53DE)     // #ba53de
+val Pink = Color(0xFFF469A9)       // #f469a9
+val Blue = Color(0xFF88BEF5)       // #88bef5
+val Peach = Color(0xFFF2A580)      // #f2a580
+val Peach2 = Color(0xFFED8756)     // #ed8756
+val Peach0 = Color(0xFFF1B79C)     // #F1B79C
+
+val Red = Color(0xFFCF4444)        // #CF4444
+val Teal = Color(0xFF60EFFF)       // #60EFFF
+val Green = Color(0xFF29F897)      // #29F897
+val LightGreen = Color(0xFF90EE90) // #90EE90
+val BabyPink = Color(0xFFFCF2FA)   // #FCF2FA
+val DarkBlue = Color(0xFF3949AB)   // #3949AB
