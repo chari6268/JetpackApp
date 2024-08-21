c9f0ca143ed83ac792183eece9a72d1f8c0bcf51
Srinivasa Chari <srinivaschari6268@gmail.com>
2024-08-21 18:08:56 +0530

    modified some files

--- app/src/main/java/com/chari6268/jetpackapp/MainActivity.kt
+++ app/src/main/java/com/chari6268/jetpackapp/MainActivity.kt
@@ -44,7 +44,7 @@ class MainActivity : ComponentActivity() {
                 Box(
                     modifier = Modifier
                         .fillMaxSize()
-                        .background(Color.DarkGray)
+                        .background(BabyPink)
                         .padding(16.dp)
                 ) {
                     Column(
@@ -61,7 +61,7 @@ class MainActivity : ComponentActivity() {
                                 .padding(vertical = 32.dp),
                             fontWeight = FontWeight.Light,
                             fontSize = 80.sp,
-                            color = Color.White,
+                            color = Color.Black,
                             maxLines = 2
                         )
                         Row(
@@ -414,11 +414,233 @@ fun BouncingIcon(offset: Offset) {
     }
 }
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
 //        Greeting("Srinivasa chari")
-    LoginScreen()
+    CalculatorPrepTheme {
+        val viewModel = viewModel<CalculatorViewModel>()
+        val state = viewModel.state
+        val buttonSpacing = 8.dp
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(BabyPink)
+                .padding(16.dp)
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.BottomCenter),
+                verticalArrangement = Arrangement.spacedBy(buttonSpacing),
+            ) {
+                Text(
+                    text = state.number1 + (state.operation?.symbol ?: "") + state.number2,
+                    textAlign = TextAlign.End,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 32.dp),
+                    fontWeight = FontWeight.Light,
+                    fontSize = 80.sp,
+                    color = Color.Black,
+                    maxLines = 2
+                )
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.spacedBy(buttonSpacing)
+                ) {
+                    CalculatorButton(
+                        symbol = "AC",
+                        color = LightGray,
+                        modifier = Modifier
+                            .aspectRatio(2f)
+                            .weight(2f)
+                    ) {
+                        viewModel.onAction(CalculatorAction.Clear)
+                    }
+                    CalculatorButton(
+                        symbol = "Del",
+                        color = LightGray,
+                        modifier = Modifier
+                            .aspectRatio(1f)
+                            .weight(1f)
+                    ) {
+                        viewModel.onAction(CalculatorAction.Delete)
+                    }
+                    CalculatorButton(
+                        symbol = "/",
+                        color = Orange,
+                        modifier = Modifier
+                            .aspectRatio(1f)
+                            .weight(1f)
+                    ) {
+                        viewModel.onAction(CalculatorAction.Operation(CalculatorOperation.Divide))
+                    }
+                }
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.spacedBy(buttonSpacing)
+                ) {
+                    CalculatorButton(
+                        symbol = "7",
+                        color = MediumGray,
+                        modifier = Modifier
+                            .aspectRatio(1f)
+                            .weight(1f)
+                    ) {
+                        viewModel.onAction(CalculatorAction.Number(7))
+                    }
+                    CalculatorButton(
+                        symbol = "8",
+                        color = MediumGray,
+                        modifier = Modifier
+                            .aspectRatio(1f)
+                            .weight(1f)
+                    ) {
+                        viewModel.onAction(CalculatorAction.Number(8))
+                    }
+                    CalculatorButton(
+                        symbol = "9",
+                        color = MediumGray,
+                        modifier = Modifier
+                            .aspectRatio(1f)
+                            .weight(1f)
+                    ) {
+                        viewModel.onAction(CalculatorAction.Number(9))
+                    }
+                    CalculatorButton(
+                        symbol = "x",
+                        color = Orange,
+                        modifier = Modifier
+                            .aspectRatio(1f)
+                            .weight(1f)
+                    ) {
+                        viewModel.onAction(CalculatorAction.Operation(CalculatorOperation.Multiply))
+                    }
+                }
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.spacedBy(buttonSpacing)
+                ) {
+                    CalculatorButton(
+                        symbol = "4",
+                        color = MediumGray,
+                        modifier = Modifier
+                            .aspectRatio(1f)
+                            .weight(1f)
+                    ) {
+                        viewModel.onAction(CalculatorAction.Number(4))
+                    }
+                    CalculatorButton(
+                        symbol = "5",
+                        color = MediumGray,
+                        modifier = Modifier
+                            .aspectRatio(1f)
+                            .weight(1f)
+                    ) {
+                        viewModel.onAction(CalculatorAction.Number(5))
+                    }
+                    CalculatorButton(
+                        symbol = "6",
+                        color = MediumGray,
+                        modifier = Modifier
+                            .aspectRatio(1f)
+                            .weight(1f)
+                    ) {
+                        viewModel.onAction(CalculatorAction.Number(6))
+                    }
+                    CalculatorButton(
+                        symbol = "-",
+                        color = Orange,
+                        modifier = Modifier
+                            .aspectRatio(1f)
+                            .weight(1f)
+                    ) {
+                        viewModel.onAction(CalculatorAction.Operation(CalculatorOperation.Subtract))
+                    }
+                }
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.spacedBy(buttonSpacing)
+                ) {
+                    CalculatorButton(
+                        symbol = "1",
+                        color = MediumGray,
+                        modifier = Modifier
+                            .aspectRatio(1f)
+                            .weight(1f)
+                    ) {
+                        viewModel.onAction(CalculatorAction.Number(1))
+                    }
+                    CalculatorButton(
+                        symbol = "2",
+                        color = MediumGray,
+                        modifier = Modifier
+                            .aspectRatio(1f)
+                            .weight(1f)
+                    ) {
+                        viewModel.onAction(CalculatorAction.Number(2))
+                    }
+                    CalculatorButton(
+                        symbol = "3",
+                        color = MediumGray,
+                        modifier = Modifier
+                            .aspectRatio(1f)
+                            .weight(1f)
+                    ) {
+                        viewModel.onAction(CalculatorAction.Number(3))
+                    }
+                    CalculatorButton(
+                        symbol = "+",
+                        color = Orange,
+                        modifier = Modifier
+                            .aspectRatio(1f)
+                            .weight(1f)
+                    ) {
+                        viewModel.onAction(CalculatorAction.Operation(CalculatorOperation.Add))
+                    }
+                }
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.spacedBy(buttonSpacing)
+                ) {
+                    CalculatorButton(
+                        symbol = "0",
+                        color = MediumGray,
+                        modifier = Modifier
+                            .aspectRatio(2f)
+                            .weight(2f)
+                    ) {
+                        viewModel.onAction(CalculatorAction.Number(0))
+                    }
+                    CalculatorButton(
+                        symbol = ".",
+                        color = MediumGray,
+                        modifier = Modifier
+                            .aspectRatio(1f)
+                            .weight(1f)
+                    ) {
+                        viewModel.onAction(CalculatorAction.Decimal)
+                    }
+                    CalculatorButton(
+                        symbol = "=",
+                        color = Orange,
+                        modifier = Modifier
+                            .aspectRatio(1f)
+                            .weight(1f)
+                    ) {
+                        viewModel.onAction(CalculatorAction.Calculate)
+                    }
+                }
+            }
+        }
+    }
 }
 
 data class User(
--- app/src/main/java/com/chari6268/jetpackapp/ui/theme/Theme.kt
+++ app/src/main/java/com/chari6268/jetpackapp/ui/theme/Theme.kt
@@ -5,6 +5,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 private val DarkColorPalette = darkColors(
         primary = Purple200,
@@ -26,22 +27,6 @@ private val LightColorPalette = lightColors(
     onSurface = Color.Black,
     */
 )
-
-@Composable
-fun JetpackAppTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
-
-    MaterialTheme(
-            colors = colors,
-            typography = Typography,
-            shapes = Shapes,
-            content = content
-    )
-}
 
 @Composable
 fun CalculatorPrepTheme(
